032023c6f38715db7f82b98eb9f872bec31e6ac8
M. Waisberg <pnemonic@gmail.com>
2020-03-26 12:37:50 +0200

    Initial commit

---
+++ app/src/main/java/com/tikal/motiondetection/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tikal.motiondetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
